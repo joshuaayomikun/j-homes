5e457753e09cbaf6cfbcb0e8e9e3e4e225162f01
Joshua <joshua.akande@moniepoint.com>
2025-07-14 00:25:09 +0100

    Moved 'fortune cookie' into module.

--- j-home.js
+++ j-home.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const fortune = require('./lib/fortune')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
-const fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-]
 
 app.use(express.static(__dirname + '/public'))
 
@@ -24,9 +18,8 @@ app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => res.render('home'))
 app.get('/about', (req, res) => {
-	const randomFortune =
-		fortunes[Math.floor(Math.random() * fortunes.length)]
-	res.render('about', { fortune: randomFortune })
+
+	res.render('about', { fortune: fortune.getFortune() })
 })
 
 /// custom 404 page
